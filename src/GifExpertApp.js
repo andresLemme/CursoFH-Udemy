ce9ce02d035a7a2ac81da2e2352a3149dfc561f1
Pocas1982 <andres.lemme@gmail.com>
2022-05-18 12:37:37 -0300

    newApp

---
+++ src/GifExpertApp.js
@@ -0,0 +1,27 @@
+
+import React, {useState} from 'react'
+
+export const GifExpertApp = () => {
+  // const categories = ['One Puch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState(['One Puch', 'Samurai X', 'Dragon Ball'])
+  
+const handleAdd = () =>{
+  setCategories([...categories, 'HunterXHunter'])
+}
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <hr/>
+      <button onClick={handleAdd}>Agregar</button>
+
+      {
+        categories.map((category) =>{
+          return(
+            <li key={category}>{category}</li>
+          )
+        })
+      }
+    </>
+  )
+}
