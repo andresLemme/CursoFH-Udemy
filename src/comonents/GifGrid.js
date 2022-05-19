5d4b97b906d0718c94cd2a54d6d7cb71ccb3f953
Pocas1982 <andres.lemme@gmail.com>
2022-05-19 07:46:26 -0300

    adduseEffect

--- src/comonents/GifGrid.js
+++ src/comonents/GifGrid.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const GifGrid = ({ category }) => {
+
+
+   useEffect(() => {
+    getGifs()
+   }, []);
+
   const getGifs = async () => {
     const url =
       "https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=CMpodpFp15TEuswOPawYe7z29S4MZK5L";
@@ -14,9 +20,11 @@ export const GifGrid = ({ category }) => {
         url: gif.images.downsized_medium.url,
       };
     });
+
     console.log(gifs);
   };
-  getGifs();
+
+  // getGifs();
   return (
     <>
       <h3>{category}</h3>
