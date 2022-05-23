67ddb78c2993d55f3b9f70f9d82a4a5f32157525
Pocas1982 <andres.lemme@gmail.com>
2022-05-23 07:40:51 -0300

    CustomHookCompleted

--- src/comonents/GifGrid.js
+++ src/comonents/GifGrid.js
@@ -1,30 +1,22 @@
 import React from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 // import { getGifs } from "../helpers/getGifs";
-// import { GifsGridItem } from "./GifsGridItem";
+import { GifsGridItem } from "./GifsGridItem";
 
 export const GifGrid = ({ category }) => {
   // const [images, setimages] = useState([]);
-  
-  const { loading} = useFetchGifs()
-  console.log(loading)
 
-  // useEffect(() => {
-  //  getGifs(category)
-  //  .then(setimages)
-  // }, [category]);
-
+  const { data: images, loading } = useFetchGifs();
 
   return (
     <>
-    {loading ? 'cargando' : 'data cargada'}
-      {/* <h3>{category}</h3>
+      <h3>{category}</h3>
+      {loading && "cargando"}
       <div className="card-grid">
-
-      {images.map(image => (
-        <GifsGridItem key={image.id} {...image} />
-      ))}
-    </div> */}
+        {images.map(image => (
+          <GifsGridItem key={image.id} {...image} />
+        ))}
+      </div>
     </>
   );
 };
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,16 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import {getGifs} from '../helpers/getGifs'
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
   const [state, setstate] = useState({
     data: [],
     loading: true
   });
-  setInterval(() => {
-    setstate({
-      data: [1,2,3,4],
-      loading: false
+
+  useEffect( () =>{
+    getGifs(category).then(image =>{
+     setTimeout(() => {
+       console.log(image)
+      setstate({
+        data: image,
+        loading:false
+      })
     })
-  }, 3000);
+     }, 3000);
+  },[category])
+
 
   return state //{data; loading:true}
 }
+
+
+ // useEffect(() => {
+  //  getGifs(category)
+  //  .then(setimages)
+  // }, [category]);
